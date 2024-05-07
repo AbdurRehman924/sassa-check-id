cad8a082b196ebe0ca4bf2d925fcf5c7c636220f
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-07 14:55:19 +0500

    Add dynamic page content fetching from WordPress API.

---
+++ pages/[id].vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mx-auto max-w-7xl my-3" v-html="data?.page.content"></div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const config = useRuntimeConfig();
+const { id } = useRoute().params;
+const props = defineProps({
+  pages: Object,
+});
+
+let pageWithId = props.pages.find((page) => page.node.link.includes(id));
+
+const { data } = await useFetch(config.public.wordpressUrl, {
+  method: "get",
+  query: {
+    query: `query GetPosts {
+  page (id:"${pageWithId.node.id}"){
+      id
+      title
+      date
+      content
+  }
+}`,
+  },
+  transform(data) {
+    return data.data;
+  },
+});
+</script>
