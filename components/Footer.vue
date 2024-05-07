16a63eb3632a485348d295090b7b92627d36bc58
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-07 14:53:47 +0500

    Add Footer and Header components. Made Footer dynamic.

---
+++ components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="bg-black text-white w-screen text-xl mx-auto p-4">
+    <p class="text-xl">SASSA CHECKER</p>
+    <div v-for="item in data" class="text-xs">
+      <a
+        v-if="item.node.isFrontPage === false"
+        :href="removeBaseUrl(item.node.link)"
+        >{{ item.node.title }}</a
+      >
+    </div>
+  </footer>
+</template>
+<script setup>
+const config = useRuntimeConfig();
+const { data } = useFetch(config.public.wordpressUrl, {
+  method: "get",
+  query: {
+    query: `query NewQuery {
+  pages {
+    edges {
+      node {
+        title
+        link
+        id
+        isFrontPage
+      }
+    }
+  }
+}`,
+  },
+  transform(data) {
+    return data.data.pages.edges;
+  },
+});
+
+const removeBaseUrl = (url) => {
+  const parts = url.split("/");
+  const lastPart = parts[parts.length - 2];
+
+  return lastPart;
+};
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="bg-[#F9C02C] w-screen text-4xl mx-auto p-4">
+    <NuxtLink to="/"> SASSA CHECKER </NuxtLink>
+  </header>
+</template>
