ff4a435e70a47a714a7a8ae4758fe9bfd3d6e6a8
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-07 14:54:20 +0500

    Refactor app.vue to use NuxtLayout and fetch pages from WordPress API.

--- app.vue
+++ app.vue
@@ -1,4 +1,29 @@
 <template>
-  <div></div>
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage :pages="data" />
+  </NuxtLayout>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+const { data } = useFetch(config.public.wordpressUrl, {
+  method: "get",
+  query: {
+    query: `query NewQuery {
+  pages {
+    edges {
+      node {
+        title
+        link
+        id
+        isFrontPage
+      }
+    }
+  }
+}`,
+  },
+  transform(data) {
+    return data.data.pages.edges;
+  },
+});
+</script>
