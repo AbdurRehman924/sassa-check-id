6e421a0e9608d20aac13495a2a8a13a31dd968be
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-11 02:22:20 +0500

    Refactor category page to use reusable components.

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="mx-auto max-w-7xl my-3 font-OpenSans">
-    <div class="flex gap-8">
-      <div
-        v-for="item in data"
-        @click="(selectedCategoryId = item.id), loadPosts(item.id)"
-        class="h-10 w-auto p-6 flex items-center rounded-full cursor-pointer hover:bg-[#f9bf2d87]"
-        :class="item.id === selectedCategoryId ? 'bg-[#f9bf2d87]' : ''">
-        {{ item.name }}
-      </div>
-    </div>
-    <div
-      v-for="item in posts"
-      class="max-w-7xl my-3 font-OpenSans text-lg mx-auto"
-      v-html="item.node.content"></div>
+    <CategoriesList :data="categories" @changeId="loadPosts" />
+    <CategoriesPosts :data="posts" />
   </div>
 </template>
 <script setup>
 const { fetchCategories } = useGetData();
 const { fetchCategoryById } = useGetData();
 
-let data = await fetchCategories();
-
-let firstCategory = computed(() => {
-  return Array.isArray(data.value) && data.value.length > 0
-    ? data.value[0].id
-    : null;
-});
-let selectedCategoryId = ref(firstCategory.value);
+let categories = await fetchCategories();
 
 let posts = ref();
 let count = ref();
@@ -37,5 +19,6 @@ const loadPosts = async (id) => {
   count.value = loadedPosts.value.count;
 };
 
-loadPosts(selectedCategoryId.value);
+let firstCategoryId = computed(() => categories.value[0].id);
+loadPosts(firstCategoryId.value);
 </script>
