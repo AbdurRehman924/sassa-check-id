1bf4e3e6fcc01401cfde15a27a3e55d02b676eec
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-11 01:42:59 +0500

    Refactored category page functionality and data fetching.

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -3,43 +3,39 @@
     <div class="flex gap-8">
       <div
         v-for="item in data"
-        @click="selectedCategoryId = item.id"
+        @click="(selectedCategoryId = item.id), loadPosts(item.id)"
         class="h-10 w-auto p-6 flex items-center rounded-full cursor-pointer hover:bg-[#f9bf2d87]"
         :class="item.id === selectedCategoryId ? 'bg-[#f9bf2d87]' : ''">
         {{ item.name }}
       </div>
     </div>
     <div
-      v-for="edge in loadedPosts?.posts?.edges"
+      v-for="item in posts"
       class="max-w-7xl my-3 font-OpenSans text-lg mx-auto"
-      v-html="edge?.node?.content"></div>
+      v-html="item.node.content"></div>
   </div>
 </template>
 <script setup>
-import { ref, watch } from "vue";
-
 const { fetchCategories } = useGetData();
 const { fetchCategoryById } = useGetData();
-const data = await fetchCategories();
+
+let data = await fetchCategories();
 
 let firstCategory = computed(() => {
-  if (Array.isArray(data.value) && data.value.length > 0) {
-    return data.value[0].id;
-  }
-  return null;
+  return Array.isArray(data.value) && data.value.length > 0
+    ? data.value[0].id
+    : null;
 });
 let selectedCategoryId = ref(firstCategory.value);
 
-let loadedPosts;
-const loadPosts = async () => {
-  if (selectedCategoryId.value !== null) {
-    loadedPosts = await fetchCategoryById(selectedCategoryId.value);
-  }
+let posts = ref();
+let count = ref();
+
+const loadPosts = async (id) => {
+  let loadedPosts = await fetchCategoryById(id);
+  posts.value = loadedPosts.value.posts;
+  count.value = loadedPosts.value.count;
 };
 
-watch(selectedCategoryId, () => {
-  loadPosts();
-});
-
-loadPosts();
+loadPosts(selectedCategoryId.value);
 </script>
