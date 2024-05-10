db5902ffe54f86a5e8d3661b3bfd930402731f6f
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-11 02:23:04 +0500

    Add category components for listing category list and displaying posts.

---
+++ components/categories/List.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex gap-8">
+    <div
+      v-for="item in data"
+      @click="(selectedCategoryId = item.id), $emit('changeId', item.id)"
+      class="h-10 w-auto p-6 flex items-center rounded-full cursor-pointer hover:bg-[#f9bf2d87]"
+      :class="item.id === selectedCategoryId ? 'bg-[#f9bf2d87]' : ''">
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  data: Object,
+});
+let selectedCategoryId = ref(props.data[0].id);
+</script>
---
+++ components/categories/Posts.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    v-for="item in data"
+    class="max-w-7xl my-3 font-OpenSans text-lg mx-auto"
+    v-html="item.node.content"></div>
+</template>
+<script setup>
+const props = defineProps({
+  data: Object,
+});
+</script>
