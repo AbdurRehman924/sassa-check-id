155bb3a6edfe80eca8b6f9c6c387724006425fca
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-05-11 18:49:52 +0500

    Fix: slug destructring

---
+++ pages/[slug].vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-if="selectedPage"
+    class="min-h-screen mx-auto my-3 max-w-7xl font-OpenSans"
+    v-html="data?.page.content"
+  ></div>
+</template>
+
+<script setup>
+  import { useRoute } from "vue-router";
+
+  const { fetchPageById } = useGetData();
+  const { fetchData } = useGetSeoData();
+
+  const route = useRoute();
+  const allPages = inject("allPages");
+
+  const selectedPage = computed(() => {
+    return allPages.value.find((page) => page.node.slug === route.params.slug);
+  });
+
+  const data = await fetchPageById(selectedPage.value.node.id);
+
+  const { title, metas, scripts } = await fetchData(
+    selectedPage.value.node.slug
+  );
+
+  useHead({
+    title,
+    meta: metas.map((meta) => ({ ...meta, hid: meta.name || meta.property })),
+    script: scripts.map((script) => ({
+      type: script.type,
+      json: script.content,
+      hid: script.type,
+    })),
+  });
+</script>
