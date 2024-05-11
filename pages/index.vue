ee38ca17138bf26b6a8054e0198f5392fa568dce
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-11 15:14:37 +0500

    Update index.vue to fetch SEO data and update head metadata.

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,21 @@
 <script setup>
 const allPages = inject("allPages");
 const { fetchPageById } = useGetData();
+const { fetchData } = useGetSeoData();
 
 let indexPage = computed(() =>
   allPages.value.find((page) => page.node.isFrontPage === true)
 );
 const data = await fetchPageById(indexPage.value.node.id);
+
+const { title, metas, scripts } = await fetchData(indexPage.value.node.slug);
+useHead({
+  title,
+  meta: metas.map((meta) => ({ ...meta, hid: meta.name || meta.property })),
+  script: scripts.map((script) => ({
+    type: script.type,
+    json: script.content,
+    hid: script.type,
+  })),
+});
 </script>
-<style></style>
