0d4bd2fab62218cd3b6bd81613271603bdd83e71
ALI HASSAN <alihassan78376@gmail.com>
2024-05-13 15:21:16 +0500

    styles added to blogs

--- pages/[uri].vue
+++ pages/[uri].vue
@@ -3,23 +3,33 @@
     <div
       v-if="data.contentTypeName == 'page' || data.contentTypeName == 'post'"
       class="min-h-screen mx-auto my-3 max-w-7xl font-OpenSans"
-      v-html="data.content"
-    ></div>
+      v-html="data.content"></div>
     <div v-else>
-      {{ data }}
+      <div v-if="data.posts" class="max-w-7xl mx-auto mt-3 mb-3">
+        <div v-for="post in data.posts" :key="post.uri">
+          <NuxtLink :to="post.uri">
+            <img
+              :src="post.featuredImage.sourceUrl"
+              :alt="post.featuredImage.altText" />
+          </NuxtLink>
+          <div v-html="post.content"></div>
+          <h2 class="mb-0">{{ post.title }}</h2>
+          <p>{{ post.date }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { imagesRegex, wordpressUrl } from "~/utils/constants";
+import { imagesRegex, wordpressUrl } from "~/utils/constants";
 
-  const route = useRoute();
-  const nuxtApp = useNuxtApp();
-  const { imagesUrl } = useRuntimeConfig().public;
-  const { data } = await useFetch(wordpressUrl, {
-    query: {
-      query: `
+const route = useRoute();
+const nuxtApp = useNuxtApp();
+const { imagesUrl } = useRuntimeConfig().public;
+const { data } = await useFetch(wordpressUrl, {
+  query: {
+    query: `
       query getPageByUri {
         nodeByUri(uri: "${route.params.uri}"){
           ... on Page{
@@ -65,44 +75,44 @@
         }
       }
       `,
-    },
-    key: route.params.uri,
-    transform(data) {
-      if (
-        data.data.nodeByUri.contentTypeName == "page" ||
-        data.data.nodeByUri.contentTypeName == "post"
-      ) {
-        const content = data.data.nodeByUri.content.replace(
-          imagesRegex,
-          imagesUrl
-        );
-        return {
-          ...data.data.nodeByUri,
-          content,
-        };
-      }
+  },
+  key: route.params.uri,
+  transform(data) {
+    if (
+      data.data.nodeByUri.contentTypeName == "page" ||
+      data.data.nodeByUri.contentTypeName == "post"
+    ) {
+      const content = data.data.nodeByUri.content.replace(
+        imagesRegex,
+        imagesUrl
+      );
       return {
         ...data.data.nodeByUri,
-        posts: data.data.nodeByUri.posts.nodes.map((post) => {
-          return {
-            ...post,
-            featuredImage: {
-              ...post.featuredImage.node,
-              sourceUrl: post.featuredImage.node.sourceUrl.replace(
-                imagesRegex,
-                imagesUrl
-              ),
-              srcSet: post.featuredImage.node.srcSet.replace(
-                imagesRegex,
-                imagesUrl
-              ),
-            },
-          };
-        }),
+        content,
       };
-    },
-    getCachedData(key) {
-      return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-    },
-  });
+    }
+    return {
+      ...data.data.nodeByUri,
+      posts: data.data.nodeByUri.posts.nodes.map((post) => {
+        return {
+          ...post,
+          featuredImage: {
+            ...post.featuredImage.node,
+            sourceUrl: post.featuredImage.node.sourceUrl.replace(
+              imagesRegex,
+              imagesUrl
+            ),
+            srcSet: post.featuredImage.node.srcSet.replace(
+              imagesRegex,
+              imagesUrl
+            ),
+          },
+        };
+      }),
+    };
+  },
+  getCachedData(key) {
+    return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+  },
+});
 </script>
