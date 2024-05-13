64b5180a3122dbb1b115b19db96a0d4028b1a5d2
ALI HASSAN <alihassan78376@gmail.com>
2024-05-13 15:28:06 +0500

    hover style changed for header

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,5 +1,7 @@
 <template>
   <header class="bg-[#F9C02C] text-4xl mx-auto p-4">
-    <NuxtLink to="/"> <h1 class="cursor-pointer">SASSA CHECKER</h1></NuxtLink>
+    <NuxtLink to="/">
+      <h1 class="cursor-pointer hover:text-black">SASSA CHECKER</h1></NuxtLink
+    >
   </header>
 </template>
