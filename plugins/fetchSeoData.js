b567e114f93757aeee1e4f4b94a608f2e30e9bb8
Abdurrehman Tariq <abdurrehmantariq8@gmail.com>
2024-05-13 18:23:34 +0500

    Cached seo data in plugin.

--- plugins/fetchSeoData.js
+++ plugins/fetchSeoData.js
@@ -1,25 +1,32 @@
 import * as cheerio from "cheerio";
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
+  const nuxtApp = useNuxtApp();
   const fetchSeoData = async (page_slug) => {
     try {
-      const response = await fetch(
-        `${config.public.domain}/wp-json/rankmath/v1/getHead?url=${config.public.domain}/${page_slug}`
+      const response = await useFetch(
+        `${config.public.domain}/wp-json/rankmath/v1/getHead?url=${config.public.domain}/${page_slug}`,
+        {
+          key: `${page_slug}_seo_data`,
+          getCachedData(key) {
+            return nuxtApp.payload.data[key] || nuxtApp.static.data[key];
+          },
+        }
       );
-      const data = await response.json();
 
-      const $ = cheerio.load(data.head);
+      const $ = cheerio.load(response.data.value.head);
       const title = $("title").text();
       const metas = $("meta")
         .toArray()
         .map((meta) => $(meta).attr());
-      //   const scripts = $("script")
-      //     .toArray()
-      //     .map((script) => {
-      //       const type = $(script).attr("type");
-      //       const content = $(script).html();
-      //       return { type, content };
-      //     });
+
+      // const scripts = $("script")
+      //   .toArray()
+      //   .map((script) => {
+      //     const type = $(script).attr("type");
+      //     const content = $(script).html();
+      //     return { type, content };
+      //   });
 
       return { title, metas };
     } catch (error) {
